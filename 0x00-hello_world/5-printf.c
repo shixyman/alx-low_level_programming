3e92bbf3d3bbe021c197dbd1b7bb3f33cbe6f57b
usri rachid <shixymanflowx@gmail.com>
2023-06-15 10:12:25 -0700

    add file C print 'message'

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - function  main for print
+ *
+ * Return: always 0 for succefully
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
