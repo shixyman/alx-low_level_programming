f522d6a5662538456ad381c3851f93add30eac5a
usri rachid <shixymanflowx@gmail.com>
2023-06-15 09:47:51 -0700

    add file C exactly "Programming is like building a multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main print puts
+ * :wq
+ * Description : this first program that will print with puts function
+ *
+ * Return :always 0 ('successfully')
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
