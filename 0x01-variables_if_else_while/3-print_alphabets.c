e490b26e5583b01b92403c57e77b4e4622000218
usri rachid <shixymanflowx@gmail.com>
2023-06-16 10:16:56 -0700

    add file program a program that prints the alphabet in lowercase, and then in uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - use Putchar print lowercase & after UPPERCASE alphapet
+ *
+ * Return: 0 (successfully)
+ *
+*/
+int main(void)
+{
+	char alphA = 'a';
+
+	while (alphA < 'z')
+	{
+		putchar(alphA);
+		alphA++;
+	}
+
+	alphA = 'A';
+
+	while (alphA <= 'Z')
+	{
+		putchar(alphA);
+		alphA++;
+	}
+
+	putchar('\n');
+	return (0);
+}
