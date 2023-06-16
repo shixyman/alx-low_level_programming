866e5821a3b28ffbbab4a4a833e430bb881042e6
usri rachid <shixymanflowx@gmail.com>
2023-06-16 12:22:04 -0700

    add file program thatprints all possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible for single digit numbers with conditions
+ * putchar without char
+ *
+ *
+ * Return: 0 (successfully)
+*/
+
+int main(void)
+{
+	int alpha = 0;
+
+	while (alpha < 10)
+	{
+		putchar(i + 48);
+		putchar(44);
+		putchar(32);
+		alpha += 1;
+	}
+	putchar('\n');
+
+	return (0);
+}
