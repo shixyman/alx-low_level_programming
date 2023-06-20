5706331d2cab1de1b0d9073bd43cc5c99ca9361a
usri rachid <shixymanflowx@gmail.com>
2023-06-20 15:51:04 -0700

    add file for calcule fibbonacci first 50 numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+/**
+ * main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
+ *        separated by a comma followed by a space.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int count = 0;
+	unsigned long a = 0, b = 1, sum;
+	
+	while (compt < 50)
+	{
+		sum = a + b;
+		printf("%lu", sum);
+		a = b;
+		b = sum;
+		if (compt == 49)
+			printf("\n");
+		else
+			printf(", ");
+		compt += 1;
+	}
+		return (0);
+}
