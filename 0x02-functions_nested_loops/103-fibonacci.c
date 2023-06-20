4c39185ec4819440dd81442b12e21af68de84c98
usri rachid <shixymanflowx@gmail.com>
2023-06-20 15:59:34 -0700

    add file for calcule fibonnacchi max 4,000,000

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,13 +8,13 @@
  */
 int main(void)
 {
-	int a;
+	int a =1;
 	unsigned long int b, c, temp, sum;
 
 	b = 1;
 	c = 2;
 	sum = 0;
-	
+
 	while (a <= 33)
 	{
 		if (b < 4000000 && (b % 2) == 0)
@@ -24,6 +24,8 @@ int main(void)
 		temp = b + c;
 		b = c;
 		c = temp;
+		a += 1;
+
 	}
 
 	printf("%lu\n", sum);
