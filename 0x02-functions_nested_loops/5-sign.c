9effdf59caa05df7c89168f7e52767733c9ca450
usri rachid <shixymanflowx@gmail.com>
2023-06-20 10:21:35 -0700

    add file program that check the signe of number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main."
+
+/**
+ * print_sign - check the sign if number is negative or positive or equal 0
+ *
+ * @s: argument of  function
+ *
+ * Return: always 0 successfully
+ */
+
+int print_sign(int s)
+{
+	if (s > o)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(30);
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-1');
+		return (-1);
+	}
+}
