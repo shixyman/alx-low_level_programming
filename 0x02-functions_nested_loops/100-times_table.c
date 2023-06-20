7978c8a9229d84202ace063923f59689f06b3e1b
usri rachid <shixymanflowx@gmail.com>
2023-06-20 15:20:40 -0700

    add file program multiplication-table, or a mathematical equation

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+
+/**
+ * print_times_table - prints the n times table, starting with 0
+ * @number: number of the times table
+ */
+void print_times_table(int number)
+{
+	int a, b, k;
+
+	if (number >= 0 && number <= 15)
+	{
+		for (a = 0; a <= number; a += 1)
+		{
+			for (b = 0; b <= number; b++)
+			{
+				c = b * i;
+				if (b == 0)
+				{
+					_putchar(c + '0');
+				} else if (c < 10 && b != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(c + '0');
+				} else if (c >= 10 && c < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((c / 10) + '0');
+					_putchar((c % 10) + '0');
+				} else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((c / 100) + '0');
+					_putchar(((c / 10) % 10) + '0');
+					_putchar((c % 10) + '0');
+				}
+			}
+				_putchar('\n');
+		}
+	}
+}
