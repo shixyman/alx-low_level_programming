80153e59df973a2076bc4e9547bb926e9452a13a
usri rachid <shixymanflowx@gmail.com>
2023-06-20 10:39:14 -0700

    add file program that  calculate absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - make absoulte value givento fucntion
+ * @number: argument of the function
+ * Return: value 0 successefully
+ */
+
+int _abs(int number)
+{
+	if (number < 0)
+	{
+		return (n * (-1));
+	}
+	else
+	{
+		return (n);
+	}
+}
