3a805487e90d9720e2f4df568b8b6146450547f9
usri rachid <shixymanflowx@gmail.com>
2023-06-20 14:32:59 -0700

    add file program  for adding a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.c"
+
+/**
+ * add - adding 2 integers and give the sum
+ * @x: argument x
+ * @y: argument y
+ * Return: sum of X and Y
+*/
+
+int add(int x, int y)
+{
+	int sum;
+
+	sum = x + y;
+
+	return (sum);
+}
