3e67a5afefe200c55220d0cb0035f9ca153d7930
usri rachid <shixymanflowx@gmail.com>
2023-06-20 09:14:02 -0700

    add file program that print the alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10 alphabet
+ *
+ * Return:0 always successuffy
+*/
+
+void print_alphabet_x10(void)
+{
+	int loop = 1;
+
+	while (loop < 10)
+	{
+		print_alphabet();
+		loop++;
+	}
+}
