f93dec66877c0c908ef34cf024c9aa362856a077
usri rachid <shixymanflowx@gmail.com>
2023-06-20 13:28:15 -0700

    add file prograam display time ad day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * jack_bauer - display minutes of day/night 24hours
+ *
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int hour = 0, min;
+
+	while (hour <= 23)
+	{
+		while (min <= 59)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(58);
+			_putchar((min / 10) + '0');
+			_putchar((min / 10) + '0');
+			_putchar('\n');
+
+			min += 1;
+		}
+		min += 1;
+	}
+}
