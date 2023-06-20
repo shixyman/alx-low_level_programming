389175fa0ce67cb1559dec352ceeedf26c0c633f
usri rachid <shixymanflowx@gmail.com>
2023-06-20 09:02:05 -0700

    add file program that print the alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,18 @@
 #include "main.h"
 
-/*
- * function print_function - display alphabet 
+/**
+ * print_alphabet - display alphabet with putchar
  *
- * Return:0 0 always successfuly
+ * Return: 0  always successfuly
 */
-
 void print_alphabet(void)
-{	
+{
 	char alphabet = 'a';
 
 	while (alphabet <= 'z')
 	{
 		_putchar (alphabet);
-		alphabet +=1 ;
+		alphabet += 1;
 	}
 	_putchar('\n');
 }
