11ff284c9ce4138ca2f86af940a0b3c8046691ab
usri rachid <shixymanflowx@gmail.com>
2023-06-21 07:44:51 -0700

    add file and fix the infinite loop

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - fix and stop infinite loop
+ * Return: 0 successfully
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	/*while (i < 10)*/
+	/*{*/
+	/*	putchar(i);*/
+	/*}*/
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
