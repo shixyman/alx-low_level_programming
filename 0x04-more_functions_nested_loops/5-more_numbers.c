3719b9c341e5f6915b25ee899c885216998c87b4
usri rachid <shixymanflowx@gmail.com>
2023-06-22 10:27:09 -0700

    add file program that display 10 rows in the row 14 numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers -display -0 to 14 many 10xtimes
+ *	with -putchar
+ * Return : always successfully 0
+ */
+
+void more_numbers(void)
+{
+	int row, number = 0;
+
+	for (row = 0 ; row < 10 ; row += 1)
+	{
+		while (number <= 14)
+		{
+			if (number >= 10)
+			{
+				_putchar((number / 10) + '0');
+			}
+			_putchar((number % 10) + '0');
+			number += 1;
+		}
+		_putchar('\n');
+	}
+}
+
