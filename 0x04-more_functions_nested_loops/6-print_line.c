117a761a5c4850b2e56a1de31aad9567f07b382f
usri rachid <shixymanflowx@gmail.com>
2023-06-22 10:52:14 -0700

    add file program that print line straight

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_line - display a line
+ * @number: the argument of the how many _ character
+ *     for line
+ */
+
+void print_line(int number)
+{
+	int number;
+
+	if (number > 0)
+	{
+		int count = 0;
+
+		while (number >= count)
+		{
+			_putchar(95);
+			count += 1;
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
