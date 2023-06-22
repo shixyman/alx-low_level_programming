d3326ec30aa342d2e3996fe7d701eb63355715ef
usri rachid <shixymanflowx@gmail.com>
2023-06-22 11:17:03 -0700

    add file program display slash with space

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - make a diagonal line with dollar sign
+ * @number: argument of many times the character
+ */
+
+void print_diagonal(int number)
+{
+	if (number > 0)
+	{
+		int row = 0, slash;
+
+		while (number >= row)
+		{
+			for (slash = 0; number >= slash; slash += 1)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
