381bb4638d1d02ee7f7d802c607f2c0b90905156
usri rachid <shixymanflowx@gmail.com>
2023-06-22 09:59:23 -0700

    add file dipslay number 0 to 9 excetp 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_most_numbers - display number 0  to 9
- * using _putchar twice
+ * using _putchar twice excetp 2 a 4
  * Return: successfully 0 always
 */
 
@@ -12,7 +12,7 @@ void print_most_umbers(void)
 
        	while (number < 10)
 	{
-		if (number != 2 && number != 4)
+		if (number != 2 || number != 4)
 		{
 			_putchar(number + '0');
 			number += 1;
