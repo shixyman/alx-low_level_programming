35064b1d019406d75ad0f35e06c13ca1c31541da
usri rachid <shixymanflowx@gmail.com>
2023-06-22 17:03:23 -0700

    add file calule prime

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
+ */
+void print_number(int number)
+{
+	if (number < 0)
+	{
+		_putchar('-');
+		number = -number;
+	}
+
+	int divisor = 1;
+	int tmp = number;
+
+	while (tmp > 9)
+	{
+		tmp /= 10;
+		divisor *= 10;
+	}
+
+	while (divisor != 0)
+	{
+		_putchar((number / divisor) + '0');
+		number %= divisor;
+		divisor /= 10;
+	}
+}
