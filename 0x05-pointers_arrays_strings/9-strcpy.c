ab64c0b6e402f4f5dd4cf861d49f95e840c68906
usri rachid <shixymanflowx@gmail.com>
2023-06-26 09:46:08 -0700

    add  file program that copies  string by pointer

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _stcpy - copy string by pointer
+ * @dest: parameter of function
+ * @src: paramter of function
+ * Return:dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int len = 0;
+
+	while (src[len] != '\0')
+	{
+		len += 1;
+	}
+	for (i = 0; i < len; i += 1)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
+
