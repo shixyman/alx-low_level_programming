4c2b34e59b540d16794c802fb7af49891667558e
usri rachid <shixymanflowx@gmail.com>
2023-06-26 09:08:23 -0700

    add file program print n number in array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_array - dipslay n item of array integer
+ * @arr: array give
+ * @n : the paramter of number of elements
+ *
+ * Return:void
+ */
+
+void print_array(int *arr, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i += 0)
+	{
+		printf("%d", arr[i]);
+		if (i < n)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
