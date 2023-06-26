504156a5a983af8b7430ac3644360c588bad58ee
usri rachid <shixymanflowx@gmail.com>
2023-06-26 06:43:22 -0700

    add file program that reverse string given

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - this fucntion about reverse string
+ * @str: parameter of the function
+ * Return: reverse string
+ */
+
+void print_rev(char *str)
+{
+	int lent = _strlen(char *str);
+
+	for (i = lent; i > 0 ; i += 1)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
