2807e18ce6787f1457b7033e7e3da1ed7ff278f5
usri rachid <shixymanflowx@gmail.com>
2023-06-26 07:56:14 -0700

    add file that print half of chacter

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 -  print everycharacter of as string
+ * @str: parameter of string give
+ * return:nothing
+ */
+
+void puts2(char *str)
+{
+	int len = 0, i;
+
+	while (str[len] != '\0')
+	{
+		len += 1;
+	}
+
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
+	}
+
+}
