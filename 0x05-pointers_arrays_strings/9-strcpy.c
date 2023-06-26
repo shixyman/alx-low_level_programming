57b2081cce3430647db36463ea9f1f6bba432103
usri rachid <shixymanflowx@gmail.com>
2023-06-26 09:56:33 -0700

    modify

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _stcpy - copy string by pointer
+ * _strcpy - copy string by pointer
  * @dest: parameter of function
  * @src: paramter of function
  * Return:dest
@@ -19,6 +19,7 @@ char *_strcpy(char *dest, char *src)
 	{
 		dest[i] = src[i];
 	}
+	dest[len] = '\0';
 	return (dest);
 }
 
