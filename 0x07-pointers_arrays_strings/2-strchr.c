e76ad0537500401aeddcb7634e00c18b53963b19
usri rachid <shixymanflowx@gmail.com>
2023-07-03 07:34:04 -0700

    locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_strchr - locates a character in a string
+ * @s: parameter to be search
+ * @c: paramter of the characterto search
+ * Return: return  NULL or index c
+ */
+
+char *_strchr(char *s, char c)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		if (s[len] == c)
+			return (len);
+		len += 1;
+	}
+
+	return ('\0');
+}
