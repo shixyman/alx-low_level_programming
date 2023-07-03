69fb69c263e8a816cbc3a360b45c5dca4604b045
usri rachid <shixymanflowx@gmail.com>
2023-07-03 04:56:08 -0700

    add file program copu memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy()- function copy the memory area
+ * @dest: parapmeter of pointer block memory
+ * @src: paramater of pointer block memory
+ * @n:paramter of number of bytes to be compared
+ * Return:return copy memory changed
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int len = 0;
+
+	while (n > 0)
+	{
+		src[len] = dest[len];
+		len += 1;
+	}
+	return (dest);
+}
