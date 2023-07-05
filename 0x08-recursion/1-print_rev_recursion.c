50298801ea12713027e9f79c2869603cc773f75c
usri rachid <shixymanflowx@gmail.com>
2023-07-05 06:01:40 -0700

    add file function reverse with recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#incluce "main.h"
+
+/**
+ * _print_rev_recursion -* display reverse of string
+ * @str: parameter of function
+ * Return: nothing void
+ */
+void _print_rev_recursion(char *str)
+{
+	if (*str)
+	{
+		_print_rev_recursion(str + 1);
+		_putchar(*str);
+	}
+}
