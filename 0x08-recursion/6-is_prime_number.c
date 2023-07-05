a20874bc9e9734da185285eeb1bf9795358954e1
usri rachid <shixymanflowx@gmail.com>
2023-07-05 08:45:20 -0700

    add file for check prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * verify_prime - check if number is prime
+ * @num: parametre of number
+ * @div: parameter of divisor
+ * Return: result 0 or -1
+ */
+int verify_prime(int num, int div)
+{
+	if (num % div == 0)
+		return (0);
+	if (num / 2 == div)
+		return (1);
+	return (verify(num, div + 1));
+}
+/**
+ * is_prime_number - checking if a number is prime
+ * @n: number be checked
+ *
+ * Return: 0 or -1
+ */
+int is_prime_number(int n)
+{
+	if (n == 2)
+		return (1);
+	return (prime_check(2, n));
+}
