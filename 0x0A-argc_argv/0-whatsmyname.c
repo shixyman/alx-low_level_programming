d41af3319bd15bd1a37fa3858a711d23c055707e
usri rachid <shixymanflowx@gmail.com>
2023-07-07 04:13:52 -0700

    program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main -display program name.
+ * @argc:parametre of main
+ * @argv:parameter of array of pointer
+ * Return: nothing 0 void
+ */
+
+int main(int argc,char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This is readme.md for 0x0A. C - argc, argv projects
