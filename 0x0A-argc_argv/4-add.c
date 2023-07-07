80ce46157b1782c2b68c985f4f506cf3eb49596c
usri rachid <shixymanflowx@gmail.com>
2023-07-07 06:43:58 -0700

    modify

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
 
 	for (len < argc)
 	{
-		for (len = 0; argv[len][count]; len += 1)
+		for (count = 0; argv[len][count]; len += 1)
 		{
 			if (!(argv[len][count] >= '0' && argv[len][count] <= '9'))
 			{
