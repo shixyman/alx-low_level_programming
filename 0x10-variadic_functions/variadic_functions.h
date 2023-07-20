570e91e0df9e7d2f9f3097e2e6c70273354cd3a6
usri rachid <shixymanflowx@gmail.com>
2023-07-20 06:19:02 -0700

    modify

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,17 +1,6 @@
 #ifndef _VARIADIC_FUNCTIONS_H_
 #define _VARIADIC_FUNCTIONS_H_
 
-#include <stdarg.h>
-/**
- * struct print - display  type with print function
- * @type: print type
- * @func: print function
- */
-typedef struct print
-{
-	char *type;
-	void (*func)(va_list);
-} print_t;
 
 int _putchar(char);
 int sum_them_all(const unsigned int n, ...);
