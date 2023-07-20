7fc0c7463ef8d50858335c5c4369928e0b0b74e5
usri rachid <shixymanflowx@gmail.com>
2023-07-19 17:08:05 -0700

    modify

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,11 +1,10 @@
-#ifndef _CALC_H_
-#define _CALC_H_
-
+#ifndef CALC_H
+#define CALC_H
 /**
  * struct op - Struct op
  *
- * @op: The operator
- * @f: The function associated
+ * @op: operator
+ * @f: function associated
  */
 typedef struct op
 {
@@ -19,5 +18,4 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
-
-#endif /* _CALC_H_ */
+#endif
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,31 +3,33 @@
 #include "3-calc.h"
 
 /**
- * main - performs simple operations
- * @argc: number of arguments passed
- * @argv: array of pointers to arguments
- *
- * Return: always 0
+ * main - compute a simple math opr
+ * @argc: number of args in argv
+ * @argv: arg containing 2 numb and opr
+ * Return: Success (0) and Error (98)
  */
 int main(int argc, char *argv[])
 {
-	int a, b, c;
-	int (*f)(int, int);
+	int num1, num2, (*fn)(int, int);
 
 	if (argc != 4)
 	{
-		printf("Error\n");
-		exit(98);
+		puts("Error");
+		return (98);
 	}
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-	f = get_op_func(argv[2]);
-	if (f == NULL)
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	if (num2 == 0 && (*argv[2] == '/' || *argv[2] == '%'))
 	{
-		printf("Error\n");
-		exit(99);
+		puts("Error");
+		return (100);
 	}
-	c = f(a, b);
-	printf("%d\n", c);
+	fn = get_op_func(argv[2]);
+	if (fn == NULL)
+	{
+		puts("Error");
+		return (99);
+	}
+	printf("%d\n", fn(num1, num2));
 	return (0);
 }
