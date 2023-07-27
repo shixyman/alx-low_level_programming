37f2f884f57281f5a224664e5193582d7375659c
usri rachid <shixymanflowx@gmail.com>
2023-07-27 08:29:52 -0700

    function that returns the number of elements in a linked list_t list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -9,6 +9,7 @@
  *
  * Description: singly linked list node structure
  */
+
 typedef struct list_s
 {
 	char *str;
@@ -17,6 +18,6 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
-
+size_t list_len(const list_t *h);
 
 #endif
