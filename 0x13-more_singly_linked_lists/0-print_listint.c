6aeadbfa73c9b6c0e91d92373854deb42453378c
usri rachid <shixymanflowx@gmail.com>
2023-08-01 07:43:28 -0700

    modify

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,21 @@
 #include "lists.h"
 #include <stdio.h>
-/**
-* print_listint - prints all the elements of a list.
-* @h: head of a list.
-* Return: numbers of nodes.
-*
-*/
 
+/**
+ * print_listint -  prints all the elements of a listint_t list.
+ * @h: list to print
+ *
+ * Return: number of elements in the list
+ */
 size_t print_listint(const listint_t *h)
 {
-		size_t len = 0;
+	size_t count = 0;
 
-		while (h != NULL)
-		{
-			printf("%d\n", h->n);
-			h = h->next;
-			len++;
-		}
-		return (len);
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,25 +1,24 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * add_nodeint - add a new node at the beginning
- * of a linked list
- * @head: this is the  head of a list.
- * @n: n is the number of element.
+ * add_nodeint - adds a new node at the beginning of a listint_t list.
+ * @head: douuble pointer to the beginning of a listint_t list
+ * @n: integer to add to the list
  *
- * Return: address of the new element. returns NUll if it fails.
+ * Return: address of new node
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-		listint_t *new;
+	listint_t *new;
 
-		new = malloc(sizeof(listint_t));
-
-		if (new == NULL)
-			return (NULL);
-
-		new->n = n;
-		new->next = *head;
-		*head = new;
-
-		return (*head);
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
 }
