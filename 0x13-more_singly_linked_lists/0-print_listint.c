f4baebccf56372a0b5bd91342d5674e493eeb9ed
usri rachid <shixymanflowx@gmail.com>
2023-08-01 09:23:12 -0700

    modify

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,15 +7,16 @@
  *
  * Return: number of elements in the list
  */
+
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 0;
+		size_t nnodes = 0;
 
-	while (h != NULL)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		count++;
-	}
-	return (count);
+		while (h != NULL)
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+			nnodes++;
+		}
+		return (nnodes);
 }
