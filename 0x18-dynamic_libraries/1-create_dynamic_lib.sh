e07494b24794828fbbc10e9167273d0a67b0c547
shixyman <shixymanflowx@gmail.com>
2023-09-18 06:02:29 -0700

    fix

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc *.c -Wall -Wextra -werror -pedantic -c -fPIC
+gcc *.c -Wall -Wextra -Werror -pedantic -c -fPIC
 gcc *.o -shared -o liball.so 
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
